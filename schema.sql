3f50bc224b645d924b4d014f7157b6192a2ac4da
T.i <timidevops@gmail.com>
2025-11-15 10:39:49 -0700

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS userdb;
-USE userdb;
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50),
-    email VARCHAR(100),
-    password VARCHAR(255)
-);
